b9f98bdcb70a1c88c7d1fd0bd417467f677bad39
sriganeshres <sriganeshthota12345@gmail.com>
2024-05-25 16:43:54 +0530

    Added Controller

---
+++ restapiapp/src/main/java/com/example/restapiapp/RestapiappApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiappApplication.class, args);
+	}
+
+}
